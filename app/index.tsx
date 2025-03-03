f739b7fbee0aedd97939960daeaf7dbca58e85b3
godie <ngugimaina2019@gmail.com>
2025-03-04 01:30:36 +0300

    add animated lottie file for the onboarding

--- app/index.tsx
+++ app/index.tsx
@@ -1,84 +1,117 @@
-import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
 import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Onboarding from "react-native-onboarding-swiper";
+import LottieView from "lottie-react-native";
+import { useRouter } from "expo-router";
 import { useAppContext } from "@/context/AppContext";
-import { Redirect } from "expo-router";
 
-const index = () => {
+const OnboardingScreen = () => {
+  const router = useRouter();
   const { user } = useAppContext();
-  const handleSocialLogin = (provider: string) => {
-    console.log(`Logging in with ${provider}`);
+
+  const handleSkip = () => {
+    if (user) {
+      router.push("/(root)/(tabs)/home");
+      return;
+    }
+    router.push("/(auth)/auth");
   };
-  return user ? (
-    <Redirect href='/(root)/(tabs)/home' />
-  ) : (
-    <ScrollView>
-      <View className=' h-screen bg-primary-300'>
-        <View className='p-2  flex-1 px-2 items-center justify-center bg-white rounded-b-[3rem]'>
-          <Image
-            className=' object-cover w-full h-full'
-            resizeMode='cover'
-            source={require("@/assets/images/logo.png")}
-          />
-          <Text className=' text-4xl text-primary-300 font-rubik-extrabold'>
-            CAMPUS CRIB
-          </Text>
-          <Text className=' text-xl font-rubik-medium'>
-            Home Hunting Made Easy
-          </Text>
-          <Text className=' text-center p-2 text-lg font-rubik-light'>
-            Sign in to campus crib
-          </Text>
-        </View>
-        <View>
-          <View className='mb-6 gap-4 p-2'>
-            <TouchableOpacity
-              activeOpacity={0.5}
-              onPress={() => handleSocialLogin("google")}
-              className='bg-white rounded-2xl flex-row items-center gap-4'
-            >
-              <Image
-                resizeMode='contain'
-                source={require("@/assets/images/google.png")}
-                style={{ width: 40, height: 40 }}
-              />
-              <Text className=' font-rubik-medium text-lg text-primary-300'>
-                sign in with google
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              activeOpacity={0.5}
-              onPress={() => handleSocialLogin("facebook")}
-              className='bg-white rounded-2xl flex-row items-center gap-4 p-2'
-            >
-              <Image
-                source={require("@/assets/images/fb.svg")}
-                style={{ width: 24, height: 24 }}
-              />
-              <Text className=' font-rubik-medium text-lg  text-primary-300'>
-                sign in with facebook
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              activeOpacity={0.5}
-              onPress={() => handleSocialLogin("github")}
-              className='bg-white p-2 rounded-2xl flex-row items-center gap-4'
-            >
-              <Image
-                source={require("@/assets/images/github.svg")}
-                style={{ width: 24, height: 24 }}
-              />
-              <Text className=' font-rubik-medium text-lg text-primary-300'>
-                sign in with github
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <Text className=' text-center p-2 text-lg font-rubik-light text-white'>
-            &copy; {new Date().getFullYear()}. all right reserved,campus crib
-          </Text>
-        </View>
-      </View>
-    </ScrollView>
+
+  const handleDone = () => {
+    if (user) {
+      router.push("/(root)/(tabs)/home");
+      return;
+    }
+    router.push("/(auth)/auth");
+  };
+  interface ControlProps {
+    onPress?: () => void;
+    disabled?: boolean;
+  }
+
+  const renderControl = (props: ControlProps, text: string) => {
+    return (
+      <TouchableOpacity
+        {...props}
+        className=' p-2 px-3 bg-primary-300  rounded-lg m-2'
+        activeOpacity={0.5}
+      >
+        <Text className=' text-white font-rubik-medium text-lg'> {text} </Text>
+      </TouchableOpacity>
+    );
+  };
+  const renderDots = (props: { isActive: boolean }) => {
+    const { isActive } = props;
+    return (
+      <View
+        className={`${
+          isActive ? "bg-white" : "bg-black-100"
+        } p-[4px] mx-1 rounded-full`}
+      />
+    );
+  };
+
+  return (
+    <Onboarding
+      onSkip={handleSkip}
+      onDone={handleDone}
+      bottomBarHeight={60}
+      SkipButtonComponent={({ ...props }) => renderControl(props, "Skip")}
+      NextButtonComponent={({ ...props }) => renderControl(props, "Next")}
+      DoneButtonComponent={({ ...props }) => renderControl(props, "Done")}
+      // DotComponent={renderDots}
+      bottomBarHighlight={false}
+      pages={[
+        {
+          backgroundColor: "#FFF",
+          image: (
+            <View>
+              <Text>helloe</Text>
+            </View>
+          ),
+          title: "Find Your Perfect Home",
+          subtitle:
+            "Easily search and find the perfect house to rent with our app.",
+        },
+        {
+          backgroundColor: "#9333ea",
+          image: (
+            <View>
+              <Text>welcome</Text>
+            </View>
+          ),
+          title: "Connect with Roommates",
+          subtitle:
+            "Find compatible roommates and share the perfect living space.",
+        },
+        {
+          backgroundColor: "#0061FF",
+          image: (
+            <View>
+              <Text>here</Text>
+            </View>
+          ),
+          title: "Safe and Secure",
+          subtitle:
+            "Ensure a safe and secure living environment with our verified listings.",
+        },
+      ]}
+      controlStatusBar={false}
+      nextLabel='Next'
+      skipLabel='Skip'
+      // doneLabel='Done'
+      containerStyles={{ paddingHorizontal: 20 }}
+      titleStyles={{ fontSize: 24, color: "#000" }}
+      subTitleStyles={{ fontSize: 16, color: "#666" }}
+    />
   );
 };
 
-export default index;
+const styles = StyleSheet.create({
+  lottie: {
+    width: 300,
+    height: 300,
+  },
+});
+
+export default OnboardingScreen;
