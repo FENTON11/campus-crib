b1e3388c7277eba57ec682046883e11a526bc53f
godie <ngugimaina2019@gmail.com>
2025-03-03 22:46:08 +0300

    add onboarding scrren first milestone

--- app/index.tsx
+++ app/index.tsx
@@ -11,49 +11,70 @@ const index = () => {
   return user ? (
     <Redirect href='/(root)/(tabs)/home' />
   ) : (
-    <ScrollView contentContainerStyle={{ flexGrow: 1, padding: 10, gap: 5 }}>
-      <View className='h-1/2 p-1'>
-        <Image
-          className=' object-cover w-full h-full'
-          resizeMode='cover'
-          source={require("@/assets/images/banner.png")}
-        />
-      </View>
-      <View className='mb-3 gap-2'>
-        <TouchableOpacity
-          activeOpacity={0.5}
-          onPress={() => handleSocialLogin("google")}
-          className='border border-primary_1  rounded-2xl flex-row items-center gap-2'
-        >
+    <ScrollView>
+      <View className=' h-screen'>
+        <View className='p-2  flex-1 px-2 items-center justify-center'>
           <Image
-            resizeMode='contain'
-            source={require("@/assets/images/google.png")}
-            style={{ width: 40, height: 40 }}
+            className=' object-cover w-full h-full'
+            resizeMode='cover'
+            source={require("@/assets/images/logo.png")}
           />
-          <Text className=' text-primary_1'>sign in with google</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={0.5}
-          onPress={() => handleSocialLogin("facebook")}
-          className=' border border-primary_1  rounded-2xl flex-row items-center gap-2 p-2'
-        >
-          <Image
-            source={require("@/assets/images/fb.svg")}
-            style={{ width: 24, height: 24 }}
-          />
-          <Text className=' text-primary_1'>sign in with facebook</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={0.5}
-          onPress={() => handleSocialLogin("github")}
-          className='border border-primary_1  rounded-2xl flex-row items-center gap-2 p-2'
-        >
-          <Image
-            source={require("@/assets/images/github.svg")}
-            style={{ width: 24, height: 24 }}
-          />
-          <Text className=' text-primary_1'>sign in with github</Text>
-        </TouchableOpacity>
+          <Text className=' text-4xl text-primary-300 font-rubik-extrabold'>
+            CAMPUS CRIB
+          </Text>
+          <Text className=' text-xl font-rubik-medium'>
+            Home Hunting Made Easy
+          </Text>
+        </View>
+        <Text className=' text-center p-2 text-lg font-rubik-light'>
+          Sign in to campus crib
+        </Text>
+        <View className='mb-6 gap-4 p-2'>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => handleSocialLogin("google")}
+            className='border border-primary-300 rounded-2xl flex-row items-center gap-4'
+          >
+            <Image
+              resizeMode='contain'
+              source={require("@/assets/images/google.png")}
+              style={{ width: 40, height: 40 }}
+            />
+            <Text className=' font-rubik-medium text-lg'>
+              sign in with google
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => handleSocialLogin("facebook")}
+            className='border p-2 border-primary-300 rounded-2xl flex-row items-center gap-4'
+          >
+            <Image
+              source={require("@/assets/images/fb.svg")}
+              style={{ width: 24, height: 24 }}
+            />
+            <Text className=' font-rubik-medium text-lg'>
+              sign in with facebook
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => handleSocialLogin("github")}
+            className='border p-2 border-primary-300 rounded-2xl flex-row items-center gap-4'
+          >
+            <Image
+              source={require("@/assets/images/github.svg")}
+              style={{ width: 24, height: 24 }}
+            />
+            <Text className=' font-rubik-medium text-lg'>
+              sign in with github
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <Text className=' text-center p-2 text-lg font-rubik-light'>
+          &copy; 2025- {new Date().getFullYear()}. all right reserved,campus
+          crib
+        </Text>
       </View>
     </ScrollView>
   );
