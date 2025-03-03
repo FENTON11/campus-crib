80a49777da3a38401fd3f2fa3ed54bedd56e1dfe
FENTON11 <fencurt99@gmail.com>
2025-03-03 17:32:08 +0300

    designed search component

--- app/(root)/(tabs)/search.tsx
+++ app/(root)/(tabs)/search.tsx
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Card } from '@/components/Cards'
+import { router } from 'expo-router'
 
 const search = () => {
+  
+  // const handleCardPress =(id:string)=> router.push(`/(root)/(stack)/property-details/${id}`);
   return (
-    <View>
-      <Text>search</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+      data={[1,2,3]}
+      numColumns={2}
+      renderItem={({item})=>(
+        <Card/>
+      )}
+      
+      />
+    </SafeAreaView>
   )
 }
 
