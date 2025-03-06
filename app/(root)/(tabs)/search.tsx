86aeaec4c885c0062ebe12722a02af329b9f7c56
godie <ngugimaina2019@gmail.com>
2025-03-06 20:13:59 +0300

    fix a bug in search page

--- app/(root)/(tabs)/search.tsx
+++ app/(root)/(tabs)/search.tsx
@@ -1,67 +1,72 @@
-import { View, Text, FlatList, ActivityIndicator, Image, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Card } from '@/components/Cards'
-import { router } from 'expo-router'
+import {
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Card } from "@/components/Cards";
+import { router } from "expo-router";
 
-import NoResults from '@/components/NoResults' 
-import Search from '@/components/Search' 
-import Filters from '@/components/Filters' 
-
+import NoResults from "@/components/NoResults";
+import Search from "@/components/Search";
+import Filters from "@/components/Filters";
 const icons = {
-  backArrow: require('@/assets/icons/backArrow.png'),
-  bell: require('@/assets/icons/bell.png'),
+  backArrow: require("@/assets/icons/back-arrow.png"),
+  bell: require("@/assets/icons/bell.png"),
 };
 
 const search = () => {
   const [loading, setLoading] = useState(false);
-  
+
   // const handleCardPress =(id:string)=> router.push(`/(root)/(stack)/property-details/${id}`);
   return (
     <SafeAreaView>
-      
       <FlatList
         // data=   {properties}
-        data={[1,2,3]}
+        data={[1, 2, 3]}
         numColumns={2}
         renderItem={({ item }) => (
           // <Card item={item} onPress={() => handleCardPress(item.$id)} />
-          <Card/>
+          <Card />
         )}
         // keyExtractor={(item) => item.$id}
-        keyExtractor={(item)=>item.toString()}
-        contentContainerClassName="pb-32"
-        columnWrapperClassName="flex gap-5 px-5"
+        keyExtractor={(item) => item.toString()}
+        contentContainerClassName='pb-32'
+        columnWrapperClassName='flex gap-5 px-5'
         showsVerticalScrollIndicator={false}
         ListEmptyComponent={
           loading ? (
-            <ActivityIndicator size="large" className="text-primary-300 mt-5" />
+            <ActivityIndicator size='large' className='text-primary-300 mt-5' />
           ) : (
             <NoResults />
           )
         }
         ListHeaderComponent={() => (
-          <View className="px-5">
-            <View className="flex flex-row items-center justify-between mt-5">
+          <View className='px-5'>
+            <View className='flex flex-row items-center justify-between mt-5'>
               <TouchableOpacity
                 onPress={() => router.back()}
-                className="flex flex-row bg-primary-200 rounded-full size-11 items-center justify-center"
+                className='flex flex-row bg-primary-200 rounded-full size-11 items-center justify-center'
               >
-                <Image source={icons.backArrow} className="size-5" />
+                <Image source={icons.backArrow} className='size-5' />
               </TouchableOpacity>
 
-              <Text className="text-base mr-2 text-center font-rubik-medium text-black-300">
+              <Text className='text-base mr-2 text-center font-rubik-medium text-black-300'>
                 Search for Your Ideal Home
               </Text>
-              <Image source={icons.bell} className="w-6 h-6" />
+              <Image source={icons.bell} className='w-6 h-6' />
             </View>
 
             <Search />
 
-            <View className="mt-5">
+            <View className='mt-5'>
               <Filters />
 
-              <Text className="text-xl font-rubik-bold text-black-300 mt-5">
+              <Text className='text-xl font-rubik-bold text-black-300 mt-5'>
                 {/* Found {properties?.length} Properties */}
                 Found Properties
               </Text>
@@ -69,9 +74,8 @@ const search = () => {
           </View>
         )}
       />
-
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default search
+export default search;
--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,6 +1,12 @@
 import { authService } from "@/appwrite/authService";
 import { User } from "@/typings";
-import React, { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { ActivityIndicator, Alert } from "react-native";
 
 const AppContext = createContext<{
@@ -11,29 +17,26 @@ const AppContext = createContext<{
   mode: "light",
 });
 const AppContextProvider = ({ children }: { children: ReactNode }) => {
-  
   const [mode, setMode] = useState<"light" | "dark">("light");
-  const [user, setUser] = useState<User | null | any>(null);
-  const [loading,setLoading] = useState(false);
-  const getUser = async ()=>{
+  const [user, setUser] = useState<User | null | any>({ username: "maina" });
+  const [loading, setLoading] = useState(false);
+  const getUser = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       const session = await authService.getUser();
-      if(session){
-        setUser(session)
+      if (session) {
+        setUser(session);
       }
-      
     } catch (error) {
-      const err = error as Error
-      return Alert.alert('Error',err?.message || "failed to get user")
+      const err = error as Error;
+      return Alert.alert("Error", err?.message || "failed to get user");
+    } finally {
+      setLoading(false);
     }
-    finally{
-      setLoading(false)
-    }
-  }
-  useEffect(()=>{
-    getUser()
-  },[])
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
   //  console.log(children)
   const share = {
     mode,
@@ -42,7 +45,10 @@ const AppContextProvider = ({ children }: { children: ReactNode }) => {
     setUser,
   };
   return (
-    <AppContext.Provider value={{ ...share }}> { loading ? <ActivityIndicator/>: children} </AppContext.Provider>
+    <AppContext.Provider value={{ ...share }}>
+      {" "}
+      {loading ? <ActivityIndicator /> : children}{" "}
+    </AppContext.Provider>
   );
 };
 
