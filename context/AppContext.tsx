79aed724d16e47f7ee2ab66b8366b711a712d0f6
godie <ngugimaina2019@gmail.com>
2025-03-11 20:03:49 +0300

    fix the initial get user

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -14,29 +14,28 @@ import { ActivityIndicator, Alert, Platform, View } from "react-native";
 type AppContextProps = {
   user: User | null;
   mode: "light" | "dark";
-  setUser:(user:User |null)=> void
-  updateUser:(user:User)=> void
-
-}
+  setUser: (user: User | null) => void;
+  updateUser: (user: User) => void;
+};
 
 const AppContext = createContext<AppContextProps>({
   user: null,
   mode: "light",
-  setUser: (user:User| null) => {},
-  updateUser: (user:User) => {}
+  setUser: (user: User | null) => {},
+  updateUser: (user: User) => {},
 });
 
 const AppContextProvider = ({ children }: { children: ReactNode }) => {
   const [mode, setMode] = useState<"light" | "dark">("light");
-  const [user, setUser] = useState<User | null >(null);
+  const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
 
-  const updateUser = (user:User)=>{
-    setUser(user)
-  }
-  const logout = ()=>{
-    setUser(null)
-  }
+  const updateUser = (user: User) => {
+    setUser(user);
+  };
+  const logout = () => {
+    setUser(null);
+  };
 
   const getUser = async () => {
     try {
@@ -44,8 +43,8 @@ const AppContextProvider = ({ children }: { children: ReactNode }) => {
       let user = await getItemFromSecureStore("campus-crib-user");
       // console.log("saved user", user);
       if (!user) {
-        const session = await authService.getUser();
-        
+        const user = await authService.getUser();
+
         console.log("user from db", user);
         if (user) {
           await saveItemToSecureStore("campus-crib-user", user);
