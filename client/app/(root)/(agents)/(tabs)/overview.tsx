e6c3709c9e48712afd6a87d036fb606543e4ed66
godie <ngugimaina2019@gmail.com>
2025-03-25 23:24:56 +0300

    add map view

--- client/app/(root)/(agents)/(tabs)/overview.tsx
+++ client/app/(root)/(agents)/(tabs)/overview.tsx
@@ -30,7 +30,7 @@ const AgentsDashboard = () => {
               </View>
             </View>
             <View className=' flex-row gap-4 items-center'>
-              <Link asChild href={"/(root)/(stack)/notifications"}>
+              <Link asChild href={"/(root)/(stack)/Notification"}>
                 <TouchableOpacity>
                   <Image source={icons.bell} className=' size-7' />
                 </TouchableOpacity>
